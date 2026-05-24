e83809acc9c339c3eb7cddb0bda0f9b4e2b8cb02
ethanmmhaugen <ehaugen@smu.edu>
2026-05-24 14:41:03 -0700

    REPL finished, ouch

--- main.c
+++ main.c
@@ -42,19 +42,19 @@ void close_input_buffer(InputBuffer* input_buffer) {
 }
 
 int main(int argc, char* argv[]) {
-    // InputBuffer* input_buffer = new_input_buffer();
+    InputBuffer* input_buffer = new_input_buffer();
 
-    // while(true) {
-    //     print_prompt();
-    //     read_input(input_buffer);
+    while(true) {
+        print_prompt();
+        read_input(input_buffer);
 
-    //     if(strcmp(input_buffer->buffer, ".exit") == 0) {
-    //         close_input_buffer(input_buffer);
-    //         exit(EXIT_SUCCESS);
-    //     } else {
-    //         printf("Unrecognized Command, '%s' .\n", input_buffer->buffer);
-    //     }
-    // }
+        if(strcmp(input_buffer->buffer, ".exit") == 0) {
+            close_input_buffer(input_buffer);
+            exit(EXIT_SUCCESS);
+        } else {
+            printf("Unrecognized Command, '%s' .\n", input_buffer->buffer);
+        }
+    }
 
     return 0;
 }
